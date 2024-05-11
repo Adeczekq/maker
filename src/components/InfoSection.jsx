ed4401a48da8d05ab433346ec650eb160fab9aeb
ADEK <adrianpodraza10@gmail.com>
2024-05-11 22:39:12 +0200

    final

--- src/components/InfoSection.jsx
+++ src/components/InfoSection.jsx
@@ -1,27 +1,30 @@
-import Item from "./Item";
-
+import Item from "./Item"
+import Passions from './../assets/illustration-passions.svg'
+import Freedom from './../assets/illustration-financial-freedom.svg'
+import Lifestyle from './../assets/illustration-lifestyle.svg'
+import Work from './../assets/illustration-work-anywhere.svg'
 function InfoSection() {
   return (
     <div className="flex flex-col gap-12 xl:flex-row xl:items-center xl:justify-center  ">
       <Item
-        imgSrc="/src/assets/illustration-passions.svg"
+        imgSrc={Passions}
         title="Indulge your passions"
         desc="Your passions shouldn't be just for the weekend. Earn a living doing what you love."
       />
       <Item
         up="xl:translate-y-14 md:-translate-x-14 xl:translate-x-0"
-        imgSrc="/src/assets/illustration-financial-freedom.svg"
+        imgSrc={Freedom}
         title="Gain financial freedom"
         desc="Start making money work for you. There’s nothing quite like earning while you sleep. "
       />
       <Item
-        imgSrc="/src/assets/illustration-lifestyle.svg"
+        imgSrc={Lifestyle}
         title="Choose your lifestyle"
         desc="Own your daily schedule. Fancy a lie-in? Go for it! Take charge of your week."
       />
       <Item
         up="xl:translate-y-14 md:-translate-x-14 xl:translate-x-0"
-        imgSrc="/src/assets/illustration-work-anywhere.svg"
+        imgSrc={Work}
         title="Work from anywhere"
         desc="Selling online means not being pinned down. Want to work AND travel? Go for it!"
       />
--- src/components/PricePanels.jsx
+++ src/components/PricePanels.jsx
@@ -1,8 +1,13 @@
+import FreeIcon from "./../assets/icon-free.svg"
+import IconPaid from "./../assets/icon-paid.svg"
+import IconCheck from "./../assets/icon-check.svg"
+import IconCheck2 from "./../assets/icon-check-second.svg"
+
 function PricePanels() {
   return (
     <div className="flex flex-col gap-24 items-center xl:flex-row xl:justify-center">
       <div className=" flex flex-col bg-secondary p-9 rounded-xl gap-4 max-w-96">
-        <img className="w-12 -mt-14 " src="src/assets/icon-free.svg" />
+        <img className="w-12 -mt-14 " src={FreeIcon} />
         <span className="text-lg font-extrabold text-white ">Dip your toe</span>
         <p className="text-grey">
           Just getting started? No problem at all! Our free plan will take you a
@@ -11,25 +16,25 @@ function PricePanels() {
         <span className="text-4xl font-extrabold text-white">Free</span>
         <div className="flex flex-col gap-4">
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check.svg" />
+            <img className="w-3 h-2 " src={IconCheck} />
             <span className="text-white">Unlimited products</span>
           </div>
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check.svg" />
+            <img className="w-3 h-2 " src={IconCheck} />
             <span className="text-white">Basic analytics</span>
           </div>
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check.svg" />
+            <img className="w-3 h-2 " src={IconCheck} />
             <span className="text-white">Limited marketplace exposure</span>
           </div>
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check.svg" />
+            <img className="w-3 h-2 " src={IconCheck} />
             <span className="text-white">10% fee per transaction</span>
           </div>
         </div>
       </div>
       <div className=" flex flex-col bg-primary p-9 rounded-xl gap-4  max-w-96">
-        <img className="w-12 -mt-16 " src="src/assets/icon-paid.svg" />
+        <img className="w-12 -mt-16 " src={IconPaid}/>
         <span className="text-lg font-extrabold text-background ">
           Dive right in
         </span>
@@ -42,21 +47,21 @@ function PricePanels() {
         </span>
         <div className="flex flex-col gap-4">
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check-second.svg" />
+            <img className="w-3 h-2 " src={IconCheck2} />
             <span className="text-background">Custom domain</span>
           </div>
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check-second.svg" />
+            <img className="w-3 h-2 " src={IconCheck2} />
             <span className="text-background">
               Advanced analytics and reports
             </span>
           </div>
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check-second.svg" />
+            <img className="w-3 h-2 " src={IconCheck2} />
             <span className="text-background">High marketplace visibility</span>
           </div>
           <div className="flex gap-5 items-center">
-            <img className="w-3 h-2 " src="src/assets/icon-check-second.svg" />
+            <img className="w-3 h-2 " src={IconCheck2} />
             <span className="text-background">5% fee per transaction</span>
           </div>
         </div>
